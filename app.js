bbd61dc6f10a18f1748988a2fc7dcca14fb92305
Eugene Zhuchenko <jekachenko@gmail.com>
2021-05-21 18:12:25 +0300

    Add server port constant

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const db = require('./db');
-const user = require('./controllers/usercontroller');
-const game = require('./controllers/gamecontroller');
+const { SERVER_PORT } = process.env;
 
 db.sync();
 app.use(require('body-parser'));
@@ -12,6 +11,7 @@ app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'));
 
 app.use('/api/game', game);
-app.listen(() => {
-  console.log('App is listening on 4000');
+
+app.listen(SERVER_PORT, () => {
+  console.log(`App is listening, http://localhost:${SERVER_PORT}`);
 });
